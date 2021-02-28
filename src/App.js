67a916ab69d5793b0c2e69d73b132e7482b0b4a4
gulyashik <guliaeva.olya2014@yandex.ru>
2021-02-28 13:41:39 +0300

    add Layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import Layout from "./hoc/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <div style = {{width:'100px', border: '2px solid black' }}>
+          <h1> Layout</h1>
+        </div>
+      </Layout>
     </div>
   );
 }
